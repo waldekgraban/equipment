b6668fd68894958410d2143f03aac63f885ce392
Waldemar Graban <waldekgraban@gmail.com>
2020-06-08 08:11:24 +0200

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,50 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+if (yn(process.env.MIX_BROWSERSYNC_ENABLED)) {
+    const target = process.env.MIX_BROWSERSYNC_TARGET || 'localhost:8000';
+
+    const host = process.env.MIX_BROWSERSYNC_HOST || 'localhost';
+    const port = process.env.MIX_BROWSERSYNC_PORT || 3000;
+
+    let config = {
+        host: host,
+        port: port,
+        proxy: {
+            target: target,
+            reqHeaders: function () {
+                return {
+                    host: `${host}:${port}`
+                };
+            }
+        }
+    };
+
+    if (yn(process.env.MIX_BROWSERSYNC_EXTENDED)) {
+        config.watchEvents = ['change', 'add'];
+        config.files = [
+            'app/**/*.php',
+            'resources/views/**/*.php',
+            'resources/assets/js/components/**/*.vue',
+            'public/js/**/*.js',
+            'public/css/**/*.css',
+        ];
+        config.ui = false;
+        config.ghostMode = false;
+    }
+
+    mix.browserSync(config);
+}
+
+function yn(value) {
+    if (value === 'false' || value === '0') {
+        return false;
+    }
+
+    if (value === 'true' || value === '1') {
+        return true;
+    }
+
+    return !!value;
+}
